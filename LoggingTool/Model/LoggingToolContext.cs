e29b42331c7694d9d778cf7b3a9c51617d52a556
Abdalla Tantawy <tant_9@yahoo.com>
2022-07-22 22:04:51 +0200

    add-models

---
+++ LoggingTool/Model/LoggingToolContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoggingTool.Model
+{
+    public class LoggingToolContext: IdentityDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Login> Logins { get; set; }
+
+
+        public LoggingToolContext(DbContextOptions<LoggingToolContext> options) : base(options)
+        {
+        }
+    }
+
+   
+}
--- LoggingTool/Program.cs
+++ LoggingTool/Program.cs
@@ -1,3 +1,7 @@
+using LoggingTool.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<LoggingToolContext>(options =>
+           options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("LocalCs")));
+builder.Services.AddIdentity<User, IdentityRole>(o => o.Password = new PasswordOptions
+{
+    RequireDigit = true,
+    RequiredLength = 8,
+    RequireLowercase = true,
+    RequireUppercase = true,
+    RequireNonAlphanumeric = false
+}).AddEntityFrameworkStores<LoggingToolContext>();
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
